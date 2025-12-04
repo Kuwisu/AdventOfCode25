f417844652ca4c468214028e92d8c54136f1eb84
Kuwisu <ckolle.jensen@gmail.com>
2025-12-05 00:06:20 +1300

    Extend code to solve day 3 part 2

--- day3/main.py
+++ day3/main.py
@@ -1,46 +1,44 @@
-import math
 import time
 
 
 FILENAME = "input.txt"
-PART = 1
+PART = 2
+BATTERIES = 2 if PART == 1 else 12
 
 
 def getMaxJoltage(bank):
     """
-    Get the maximum joltage of a battery bank by turning on two batteries. The batteries that are turned on are
-    concatenated to form the joltage output: for example, the maximum joltage output of bank "28134" is 84, as the 2nd
-    battery '8' and the 5th battery '4' are turned on.
+    Get the maximum joltage of a battery bank by turning on a set amount of batteries. The batteries that are turned on
+    are concatenated to form the joltage output: for example, the maximum joltage output of bank "28134" when turning on
+    two batteries is 84, as the 2nd battery '8' and the 5th battery '4' are turned on.
 
     :param bank: a numeric string, with each character representing a battery
     :return: the maximum joltage output of the battery bank
     """
-    max_joltage = 0
-    joltage_index = 0
-    # Select first battery
-    for i in range(len(bank) - 1):
-        current_joltage = int(bank[i]) * 10
-        if current_joltage > max_joltage:
-            max_joltage = current_joltage
-            joltage_index = i
+    max_joltage = ["0"] * BATTERIES
 
-        if max_joltage == 90:
-            break
+    current_idx = -1
+    for i in range(BATTERIES):
+        # The last valid position is such that every battery after it in the bank would be selected.
+        last_valid_position = len(bank) - ((BATTERIES - i) - 1)
+        for j in range(current_idx + 1, last_valid_position):
+            if int(bank[j]) > int(max_joltage[i]):
+                max_joltage[i] = bank[j]
+                current_idx = j
 
-    # Select second battery to the right of the first one
-    for j in range(joltage_index + 1, len(bank)):
-        # Round max joltage down to nearest 10 before adding current joltage
-        current_joltage = int(math.floor(max_joltage / 10)) * 10 + int(bank[j])
-        if current_joltage > max_joltage:
-            max_joltage = current_joltage
+            if max_joltage[i] == "9":
+                break
 
-        if max_joltage == 99:
-            break
-
-    return max_joltage
+    return int("".join(max_joltage))
 
 
 def __main__(sequence):
+    """
+    For a sequence of battery banks, get the maximum joltage of each and sum them together.
+
+    :param sequence: a list of numeric strings representing battery banks
+    :return: the sum of the battery banks' maximum joltage outputs
+    """
     total_output = 0
     for bank in sequence:
         total_output += getMaxJoltage(bank)
