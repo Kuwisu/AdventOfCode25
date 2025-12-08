3f3898b8a920143f4610a2b1e082e1a398bfc4a8
Kuwisu <ckolle.jensen@gmail.com>
2025-12-09 12:32:19 +1300

    Solve day 4 part 2

--- day4/main.py
+++ day4/main.py
@@ -3,7 +3,7 @@ import time
 from day4.adjacentTiles import getDirectionChecks
 
 FILENAME = "input.txt"
-PART = 1
+PART = 2
 
 
 def getIsAccessible(grid, x, y):
@@ -27,18 +27,47 @@ def getIsAccessible(grid, x, y):
 
 def main(grid):
     """
-    Compute the number of rolls of paper accessible to the forklift in a grid.
+    For part 1, compute the number of rolls of paper accessible to the forklift in a grid.
+    For part 2, compute the number of rolls that can be removed by the Elves.
 
-    :param grid: the grid of paper rolls as a list of strings
+    :param grid: the grid of paper rolls as a list of strings, with a . corresponding to an empty space and an @ corresponding to a paper roll.
     :return: the number of accessible rolls of paper
     """
     num_accessible = 0
+    num_removed = 0
+    roll_locations = []
     for x in range(len(grid)):
         for y in range(len(grid[0])):
-            if grid[x][y] != ".":
-                num_accessible += getIsAccessible(grid, x, y)
+            if grid[x][y] == ".":
+                continue
 
-    return num_accessible
+            roll_accessible = getIsAccessible(grid, x, y)
+            if roll_accessible:
+                num_accessible += 1
+                if PART == 2:
+                    # Replace the roll of paper in the grid with an empty space.
+                    grid[x] = f"{grid[x][:y]}.{grid[x][y+1:]}"
+                    num_removed += 1
+            else:
+                roll_locations.append((x, y))
+
+    if PART == 1:
+        return num_accessible
+
+    # Repeatedly check the accessibility of rolls until all rolls are iterated through with no removal.
+    removing_rolls = True
+    while removing_rolls:
+        removing_rolls = False
+        for x, y in roll_locations:
+            roll_accessible = getIsAccessible(grid, x, y)
+            if roll_accessible:
+                # Replace the roll of paper in the grid with an empty space.
+                grid[x] = f"{grid[x][:y]}.{grid[x][y+1:]}"
+                roll_locations.remove((x, y))
+                num_removed += 1
+                removing_rolls = True
+
+    return num_removed
 
 
 if __name__ == "__main__":
